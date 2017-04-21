39b70f1190af9e1ea8aadb7c3bb32a51d8d3be0b
Usman Sheikh <sh.usmanikram@hotmail.com>
2017-04-22 02:12:00 +0500

    Add project files.

---
+++ DemoTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoTest.MvcApplication" Language="C#" %>
---
+++ DemoTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoTest.Startup))]
+namespace DemoTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
